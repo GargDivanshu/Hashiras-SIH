01e533d0e9f7d0739abf9988967f526680d8eb56
Junaid <95287515+theonlyjunaid@users.noreply.github.com>
2023-09-10 13:47:27 +0530

    Add files via upload

---
+++ supplier/app/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { View, Text, StyleSheet, SafeAreaView } from "react-native";
+import HomeComponent from '../components/home/HomeComponent';
+
+
+const Home = () => {
+
+    return (
+        <SafeAreaView>
+            <View style={styles.container}>
+                <Text style={styles.text}>
+                    This is Home Screen For Supplier
+                </Text>
+            <HomeComponent/>
+            </View>
+        </SafeAreaView>
+    );
+};
+const styles = StyleSheet.create({
+    container: {
+        alignContent: 'center',
+        justifyContent: 'center',
+        height: '100%'
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+});
+
+export default Home;
+
