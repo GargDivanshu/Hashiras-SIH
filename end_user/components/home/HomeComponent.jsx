baf184cc2a33676e85e7ee9098063ba1d196157e
Junaid <95287515+theonlyjunaid@users.noreply.github.com>
2023-09-10 13:46:38 +0530

    Add files via upload

---
+++ end_user/components/home/HomeComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from "react-native";
+
+const HomeComponent = () => {
+  return (
+    <View>
+        <Text style={{textAlign:"center"}}>this Home component from HomeComponent.jsx</Text>
+    </View>
+  )
+}
+
+export default HomeComponent
